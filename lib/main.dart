b69bc06bb2d434e8fa052f576188a1d038d52315
Andrey <artvik50@gmail.com>
2025-01-22 15:16:58 +0300

    feat: add page home to routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,6 @@
+import 'package:app/pages/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Home(),
-  ));
-}
-
-class Home extends StatelessWidget {
-  const Home({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-      title: Text(
-        'Initial commit',
-        style: TextStyle(fontSize: 20.0, color: Colors.cyan),
-      ),
-    ));
-  }
+  runApp(MaterialApp(initialRoute: '/', routes: {'/': (context) => Home()}));
 }
